f57674ac8d36bdf9b11736c8a4dc7e02402efd90
Varun Magesh <vim@law.stanford.edu>
2024-10-03 10:01:26 -0700

    add export

--- cacafo/export.py
+++ cacafo/export.py
@@ -2,44 +2,62 @@ import csv
 import datetime
 import json
 
+import geoalchemy2 as ga
+import geopandas as gpd
 import rich_click as click
 import rl.utils.io
-from sqlalchemy import select
+import sqlalchemy as sa
 from sqlalchemy.orm import Session
 from tqdm import tqdm
 
-from cacafo.db.sa_models import ConstructionAnnotation, County, Facility
+import cacafo.db.sa_models as m
+
+EXPORTERS = {}
 
 
-def export_csv(session: Session, output_path: str):
+def exporter(entity: str, format_: str):
+    def decorator(func):
+        func.entity = entity
+        func.format = format_
+        EXPORTERS[entity] = EXPORTERS.get(entity, {}) | {format_: func}
+        return func
+
+    return decorator
+
+
+@exporter("facilities", "csv")
+def facilities_csv(session: Session, output_path: str):
     facility_id_to_county = {
-        row.id: County.geocode(session, lon=row.longitude, lat=row.latitude).name
+        row.id: m.County.geocode(session, lon=row.longitude, lat=row.latitude).name
         for row in session.execute(
-            select(
-                Facility.id,
-                Facility.geometry.ST_X().label("longitude"),
-                Facility.geometry.ST_Y().label("latitude"),
+            sa.select(
+                m.Facility.id,
+                m.Facility.geometry.ST_X().label("longitude"),
+                m.Facility.geometry.ST_Y().label("latitude"),
             )
         ).all()
     }
 
     rows = session.execute(
-        select(
-            Facility.id.label("facility_id"),
-            Facility.hash.label("facility_uuid"),
-            Facility.geometry.ST_Centroid().ST_X().label("longitude"),
-            Facility.geometry.ST_Centroid().ST_Y().label("latitude"),
-            ConstructionAnnotation.id.label("construction_annotation_id"),
-            ConstructionAnnotation.construction_lower_bound,
-            ConstructionAnnotation.construction_upper_bound,
-            ConstructionAnnotation.destruction_lower_bound,
-            ConstructionAnnotation.destruction_upper_bound,
-            ConstructionAnnotation.significant_population_change,
-            ConstructionAnnotation.is_primarily_indoors.label("indoor_outdoor"),
-            ConstructionAnnotation.has_lagoon,
+        sa.select(
+            m.Facility.id.label("facility_id"),
+            m.Facility.hash.label("facility_uuid"),
+            m.Facility.geometry.ST_Centroid().ST_X().label("longitude"),
+            m.Facility.geometry.ST_Centroid().ST_Y().label("latitude"),
+            m.ConstructionAnnotation.id.label("construction_annotation_id"),
+            m.ConstructionAnnotation.construction_lower_bound,
+            m.ConstructionAnnotation.construction_upper_bound,
+            m.ConstructionAnnotation.destruction_lower_bound,
+            m.ConstructionAnnotation.destruction_upper_bound,
+            m.ConstructionAnnotation.significant_population_change,
+            m.ConstructionAnnotation.is_primarily_indoors.label("indoor_outdoor"),
+            m.ConstructionAnnotation.has_lagoon,
         )
-        .join(ConstructionAnnotation, Facility.id == ConstructionAnnotation.facility_id)
-        .where(Facility.archived_at.is_(None))
+        .join(
+            m.ConstructionAnnotation,
+            m.Facility.id == m.ConstructionAnnotation.facility_id,
+        )
+        .where(m.Facility.archived_at.is_(None))
     ).all()
 
     rows = [row._asdict() for row in rows]
@@ -52,8 +70,9 @@ def export_csv(session: Session, output_path: str):
     return rows
 
 
+@exporter("facilities", "geojson")
 def export_geojson(session: Session, output_path: str):
-    query = select(Facility).where(Facility.archived_at.is_(None))
+    query = sa.select(m.Facility).where(m.Facility.archived_at.is_(None))
     facilities = session.execute(query).scalars().all()
     features = [facility.to_geojson_feature() for facility in tqdm(facilities)]
     geojson = {
@@ -65,36 +84,67 @@ def export_geojson(session: Session, output_path: str):
     return geojson
 
 
+@exporter("parcels", "csv")
+def export_parcels_geojson(session: Session, output_path: str):
+    query = sa.select(m.Parcel).options(
+        sa.orm.joinedload(m.Parcel.county, innerjoin=True)
+    )
+    parcels = session.execute(query).scalars().all()
+    df = gpd.GeoDataFrame(
+        {
+            "id": [parcel.id for parcel in parcels],
+            "geometry": [
+                parcel.inferred_geometry and ga.shape.to_shape(parcel.inferred_geometry)
+                for parcel in parcels
+            ],
+            "number": [parcel.number for parcel in parcels],
+            "county": [parcel.county.name for parcel in parcels],
+        }
+    )
+    text = df.to_json()
+    with open(output_path, "w") as f:
+        f.write(text)
+    return df
+
+
 @click.command("export")
 @click.option(
     "--output-path",
     "-o",
-    default=str(
-        rl.utils.io.get_data_path()
-        / f"{datetime.datetime.now().strftime('%Y-%m-%d')}.geojson"
-    ),
+    default="",
     help="Output path",
 )
 @click.option(
     "--format",
     "-f",
     "format_",
-    default=None,
+    default="",
     help="Output format; inferred from output path if not provided",
 )
-def _cli(output_path: str, format_: str | None):
+@click.argument("entity", type=click.Choice(EXPORTERS.keys()))
+def _cli(entity, output_path, format_):
     from cacafo.db.session import get_sqlalchemy_session
 
-    if format_ is None:
-        format_ = output_path.split(".")[-1]
+    if not format_:
+        if not output_path:
+            # check if there's only one format for the entity
+            formats = EXPORTERS[entity].keys()
+            if len(formats) == 1:
+                format_ = next(iter(formats))
+            else:
+                raise click.UsageError(
+                    "Output format must be provided when output path is not provided"
+                )
+        else:
+            format_ = output_path.suffix[1:]
+
+    if not output_path:
+        output_path = (
+            rl.utils.io.get_data_path()
+            / f"{entity}_{datetime.datetime.now().strftime('%Y-%m-%d')}.{format_}"
+        )
 
     with get_sqlalchemy_session() as session:
-        match format_:
-            case "geojson":
-                export_geojson(session, output_path)
-            case "csv":
-                export_csv(session, output_path)
-            case _:
-                raise ValueError(f"Unknown format: {format_}")
-
-    click.echo(f"Exported data to {output_path}")
+        exporter = EXPORTERS[entity][format_]
+        result = exporter(session, output_path)
+        click.echo(f"Exported {len(result)} records to {output_path}")
